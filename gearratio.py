a4eac765d206e383b5e6ecac6d544ba239d50e98
JordanRichard <jricha23@unb.ca>
2023-12-14 09:02:54 -0400

    Number extraction completed

--- gearratio.py
+++ gearratio.py
@@ -1,6 +1,7 @@
 with open('input.txt') as fp:
 
     powers = []
+    sum = 0
 
     #   Feed input data into a 2d array
     for line in fp.readlines():
@@ -11,21 +12,35 @@ with open('input.txt') as fp:
 for ridx,row in enumerate(powers):
     print(row)
  
-    #   Reset L/R markers for new row
+    #   Reset L/R markers / number stack for new row
     l_marker = None
     r_marker = None
+    number_stack = ""
 
-    for cidx,column in enumerate(row):
+    for cidx, column_value in enumerate(row):
 
         #   If we found a numer, mark its index
-        if column.isnumeric():
+        if column_value.isnumeric():
 
             #   Mark the LHS coordinate of this number sequence, else mark/expand the RHS
             if l_marker is None:
-                pass
+                l_marker = (ridx,cidx)
+                number_stack += column_value
 
             else:
                 #   Check if the next column is empty (.) or non-numeric. If so, RHS is found. (Then, check for adjacent symbols)
+                if cidx + 1 < len(row) and not row[cidx + 1].isnumeric():
+                    number_stack += column_value
+                    print(f"End of this number reached: {number_stack}")
+
+                    #   Clear the stack
+                    number_stack = ""
+
+
+                #   Else, we are partway through the number, continue building it
+                else:
+                    number_stack += column_value
+
                 pass
 
 
